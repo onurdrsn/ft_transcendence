3ed61545001d5dc5d196f911bee37362bfe34efe
onur55-tr <onurdrsn55@gmail.com>
2024-03-09 13:49:22 +0300

    up

--- srcs/requirements/main/main/asgi.py
+++ srcs/requirements/main/main/asgi.py
@@ -1,21 +1,25 @@
 """Asynchronous Server Gateway Interface"""
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from tictac.routing import websocket_urlpatterns as tictac
 from chat.routing import websocket_urlpatterns as chat
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+from pong.routing import websocket_urlpatterns as pong
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
-                tictac + chat
+                tictac + chat + pong
             )
         ),
     ),
 })
+
--- srcs/requirements/main/main/urls.py
+++ srcs/requirements/main/main/urls.py
@@ -1,23 +1,31 @@
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
-from main.views import *
+from .views import *
 from django.contrib.auth.views import *
+from django.utils.translation import gettext as _
+from pong.views import fetch_rooms
+from django.views.static import serve
 
 urlpatterns = [
-    path('', include('account.urls', namespace='account')),
-    path('admin/', admin.site.urls, name="admin"),
-    path('chat/', include('chat.urls', namespace='chat')),
-    path('friend/', include('friend.urls', namespace='friend')),
-    path('tictac/', include('tictac.urls', namespace='tictac')),
-    path('pong/', include('pong.urls', namespace='pong')),
-    path('changelog', changelog, name='changelog'),
+	re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+	re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+	path('', include('account.urls', namespace='account')),
+	path('admin/', admin.site.urls, name="admin"),
+	path('chat/', include('chat.urls', namespace='chat')),
+	path('friend/', include('friend.urls', namespace='friend')),
+	path('pong/', include('pong.urls', namespace='pong')),
+	path('tictac/', include('tictac.urls', namespace='tictac')),
+	path('fetch-rooms/', fetch_rooms, name='fetch_rooms'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += i18n_patterns(
+#     *urlpatterns,
+#     prefix_default_language=False
+# )
 
 urlpatterns += [
     path('i18n/setlang/', set_language, name='set_language'),
@@ -25,15 +33,16 @@ urlpatterns += [
 
 urlpatterns += [
     path('change/done/', PasswordChangeDoneView.as_view(template_name='password/change_done.html'),
-        name='password_change_done'),
+         name='password_change_done'),
     path('change/', PasswordChangeView.as_view(template_name='password/change.html'),
-        name='password_change'),
+         name='password_change'),
     path('reset/done/', PasswordResetCompleteView.as_view(template_name='password/reset_done.html'),
-        name='password_reset_done'),
+         name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/', PasswordResetMyView.as_view(), name='password_reset'),
     path('complete/', PasswordResetCompleteView.as_view(template_name='password/reset_complete.html'),
-        name='password_reset_complete'),
+         name='password_reset_complete'),
 ]
 
 handler404 = 'main.views.handler404'
+
