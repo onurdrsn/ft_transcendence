e8a193c8f3a5f983b3174952910eeaf3c9d51153
Onur Dursun <58361733+onurdrsn@users.noreply.github.com>
2024-05-24 14:17:36 +0300

    Update start.sh

--- srcs/requirements/start.sh
+++ srcs/requirements/start.sh
@@ -5,19 +5,28 @@
 #  exit 1
 #fi
 
+DJANGO_SETTINGS_MODULE=main.settings
+export DJANGO_SETTINGS_MODULE
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "postgres" -U "$POSTGRES_USER" -c '\q'; do
+    echo "PostgreSQL is unavailable - sleeping"
+    sleep 1
+done
+
 # For running docker compose
 if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
-    python3 main/manage.py createcachetable
-    python3 main/manage.py makemigrations --noinput
-    python3 main/manage.py migrate --noinput
-    python3 main/manage.py createsuperuser \
+    # python3 manage.py createcachetable
+    python3 manage.py makemigrations --noinput
+    python3 manage.py migrate --noinput
+    python3 manage.py collectstatic --noinput
+    python3 manage.py createsuperuser \
         --noinput \
+        --first_name "$DJANGO_SUPERUSER_FIRSTNAME" \
+        --last_name "$DJANGO_SUPERUSER_LASTNAME" \
         --username "$DJANGO_SUPERUSER_USERNAME" \
         --email "$DJANGO_SUPERUSER_EMAIL"
-    #python3 main/manage.py runserver_plus --cert-file key/cert.pem --key-file key/key.pem
-    if [ "$1" ]; then
-      python3 main/manage.py runserver "$1"
-    fi
+    python3 manage.py compilemessages --ignore=venv
+    # django-admin check
+    daphne -b 0.0.0.0 -p 8000 main.asgi:application
 
 # Just to run the script
 elif [ -f ".env" ]; then
@@ -41,15 +50,19 @@ elif [ -f ".env" ]; then
     username="$(echo "$str" | awk -F '=' '{print $2}' | awk '{print $1}')"
     mail="$(echo "$str" | awk -F '=' '{print $3}'| awk '{print $1}')"
     echo -e "username='$username' mail ='$mail'"
-    python3 main/manage.py createcachetable
-    python3 main/manage.py migrate --noinput
-    python3 main/manage.py makemigrations --noinput
-    python3 main/manage.py createsuperuser \
+    python3 manage.py makemigrations --noinput
+    python3 manage.py migrate --noinput
+    python3 manage.py collectstatic --noinput
+    python3 manage.py createsuperuser \
         --noinput \
-        --username "$username" \
-        --email "$mail"
-    #python3 main/manage.py runserver_plus --cert-file key/cert.pem --key-file key/key.pem
-    python3 main/manage.py runserver "$1"
+        --first_name "$DJANGO_SUPERUSER_FIRSTNAME" \
+        --last_name "$DJANGO_SUPERUSER_LASTNAME" \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL"
+    #python3 manage.py runserver_plus --cert-file key/cert.pem --key-file key/key.pem
+    python3 manage.py compilemessages --ignore=venv
+    daphne -b 0.0.0.0 -p 8000 main.asgi:application
+
 else
     echo -e "\e[31mUsername not found\e[0m"
     exit 1
